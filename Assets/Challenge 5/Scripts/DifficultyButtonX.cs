8a50e815c6b64672b79ccb8f62a52c037e5b9eef
KarlisGP <karlis.plavenieki@gmail.com>
2026-05-24 22:33:39 +0300

    Up to step 6.

--- Assets/Challenge 5/Scripts/DifficultyButtonX.cs
+++ Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -7,9 +7,8 @@ public class DifficultyButtonX : MonoBehaviour
 {
     private Button button;
     private GameManagerX gameManagerX;
-    public int difficulty;
+    public int difficulty; // This should be 1, 2, or 3 (set in the Inspector)
 
-    // Start is called before the first frame update
     void Start()
     {
         gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
@@ -17,15 +16,10 @@ public class DifficultyButtonX : MonoBehaviour
         button.onClick.AddListener(SetDifficulty);
     }
 
-    /* When a button is clicked, call the StartGame() method
-     * and pass it the difficulty value (1, 2, 3) from the button 
-    */
     void SetDifficulty()
     {
         Debug.Log(button.gameObject.name + " was clicked");
-        gameManagerX.StartGame();
+        // Pass the difficulty value to the StartGame method
+        gameManagerX.StartGame(difficulty);
     }
-
-
-
 }
